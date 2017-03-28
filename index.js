109f0d093f22e8d622ddc0112fa69e75ab502a80
natansevero <natanseverolp@gmail.com>
2017-03-28 11:44:23 -0300

    First Deploy

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+	res.status(200).json({ "API STATUS": "API Node.js para aplicativo SharePhoto" })
+})
+
 app.listen(app.get('port'), () => {
 	console.log(`Aplicação rodando na porta ${app.get('port')}`);
 });
