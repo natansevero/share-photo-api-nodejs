d8bf4bb25219eb87ee3c3e706d01f0ab383049db
natansevero <natanseverolp@gmail.com>
2017-03-29 15:34:57 -0300

    Implementado cors da aplicação

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,13 @@
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 module.exports = app => {
   app.set('port', process.env.PORT || 3000);
 
+  app.use(cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  }));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
 }
