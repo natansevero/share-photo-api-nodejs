a1301174654d9a539689dc727e5e72dc68bb096f
natansevero <natanseverolp@gmail.com>
2017-03-29 23:43:36 -0300

    Implementando autenticação e Deploy 2

--- libs/db.js
+++ libs/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-//var url = "mongodb://natansevero:silva2011@ds145380.mlab.com:45380/share-photo";
-var url = "mongodb://localhost/share-photo";
+var url = "mongodb://natansevero:silva2011@ds145380.mlab.com:45380/share-photo";
+// var url = "mongodb://localhost/share-photo";
 var single_connection;
 
 module.exports = app => {
